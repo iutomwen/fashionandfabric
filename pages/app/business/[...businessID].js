c18997a6847078d0529b6ea4c6f8ee67c9a88cf6
Williams <iutomwen@gmail.com>
2021-10-22 08:48:17 +0100

    new

--- pages/app/business/[...businessID].js
+++ pages/app/business/[...businessID].js
@@ -1,7 +1,48 @@
 import React from "react";
+import AppLayout from "../../../components/layouts/AppLayout";
+import {
+  Box,
+  Breadcrumbs,
+  Button,
+  Card,
+  CardContent,
+  CardHeader,
+  Container,
+  Divider,
+  Grid,
+  Link,
+  TextField,
+  Typography,
+} from "@mui/material";
+import NextLink from "next/link";
 
-function businessID() {
-  return <div></div>;
+function BusinessID() {
+  return (
+    <AppLayout>
+      <Box
+        sx={{
+          backgroundColor: "background.default",
+          minHeight: "100%",
+          py: 3,
+        }}
+      >
+        <Breadcrumbs aria-label="breadcrumb">
+          <NextLink href="/app/dashboard" passHref>
+            <Link underline="hover" color="inherit">
+              Dashboard
+            </Link>
+          </NextLink>
+          <NextLink href="/app/business" passHref>
+            <Link underline="hover" color="inherit">
+              Business Users
+            </Link>
+          </NextLink>
+          <Typography color="text.primary">Create</Typography>
+        </Breadcrumbs>
+        <Container>Business Id</Container>
+      </Box>
+    </AppLayout>
+  );
 }
 
-export default businessID;
+export default BusinessID;
