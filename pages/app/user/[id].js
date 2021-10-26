ff6f0215a5b5d1dcf6173a088d23aa7b02df1d26
Williams <iutomwen@gmail.com>
2021-10-27 00:37:39 +0100

    add

--- pages/app/user/[id].js
+++ pages/app/user/[id].js
@@ -3,27 +3,27 @@ import React from 'react'
 function UserEdit() {
     
     return (
-        
-            <AppLayout>
-    <ToastNotify />
-    <CssBaseline />
-    <Head>
-      <title>{APPNAME} - Edit Profile</title>
-      <link rel="icon" href="/favicon.ico" />{" "}
-    </Head>
-    <Box
-        className="mt-5 ml-0 md:ml-5 xl:ml-10 relative"
-        sx={{
-          backgroundColor: "background.default",
-          minHeight: "100%",
-          py: 3,
-          px: 2,
-        }}
-      >
+        <div></div>
+    //         <AppLayout>
+    // <ToastNotify />
+    // <CssBaseline />
+    // <Head>
+    //   <title>{APPNAME} - Edit Profile</title>
+    //   <link rel="icon" href="/favicon.ico" />{" "}
+    // </Head>
+    // <Box
+    //     className="mt-5 ml-0 md:ml-5 xl:ml-10 relative"
+    //     sx={{
+    //       backgroundColor: "background.default",
+    //       minHeight: "100%",
+    //       py: 3,
+    //       px: 2,
+    //     }}
+    //   >
 
-          </Box>
+    //       </Box>
 
-        </AppLayout>
+    //     </AppLayout>
     )
 }
 
